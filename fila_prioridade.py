ed04f00a4f554af8f0a4d6bbbde70f19a94fb765
Raphael Chagas <rapha.ch@hotmail.com>
2023-07-18 21:08:50 -0300

    add e remove

--- fila_prioridade.py
+++ fila_prioridade.py
@@ -19,34 +19,56 @@ class FilaPrioridade:
     # Retorna True se a fila de prioridade está vazia, False caso contrário
     def is_empty(self) -> bool:
         # implementação do método
-        pass
+        return self.__qtdItens == 0
 
     
     # retorna True se a fila de prioridade está cheia, False caso contrário
     def is_full(self) -> bool:
         # implementação do método
-        pass
+        return self.__qtdItens >= self.__capacidade
 
 
     # Retorna uma referência para o primeiro item da fila de prioridade
     # Caso a lista esteja vazia, retorna None
     def first(self) -> No:
         # implementação do método
-        pass
+        return self.__inicio
 
 
     # insere um item na fila de prioridade e retorna True, se o item for inserido
     # se a fila de prioridade estiver cheia, lança uma exceção: raise Exception("mensagem de erro")
     def add(self, valor, prioridade) -> bool:
         # implementação do método
-        pass
+        no = No(valor, prioridade)
+        if self.is_full():
+            raise Exception("Erro!")
+        if self.is_empty():
+            self.__inicio = no
+        elif no.prioridade>self.__inicio.prioridade:
+            no.prox = self.__inicio
+            self.__inicio = no
+        else:
+            iterar = self.__inicio
+            while iterar.prox is not None and no.prioridade <= iterar.prox.prioridade:
+                iterar = iterar.prox
+            no.prox = iterar.prox
+            iterar.prox = no
+        self.__qtdItens += 1
+        return True
+        
 
     
     # remove o primeiro item da fila de prioridade, caso não esteja vazia, e retorna o Nó
     # se a fila de prioridade estiver vazia, lança uma exceção: raise Exception("mensagem de erro")
     def remove(self) -> No:
         # implementação do método
-        pass
+        if self.is_empty():
+            raise Exception("Erro!")
+        else:
+            iterar = self.__inicio
+            self.__inicio = self.__inicio.prox
+            self.__qtdItens -= 1
+            return iterar
 
 
     # retorna uma lista de tuplas com os itens (valor e prioridade) da fila de prioridade 
@@ -56,7 +78,6 @@ class FilaPrioridade:
     def display(self) -> list[tuple()]:
         # implementação do método
         pass
-    
 
     # retorna a quantidade de elementos na fila de prioridade
     # se a fila de prioridade estiver vazia, retorna ZERO
